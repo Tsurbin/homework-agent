db361b39f9d55e46f534baf80aee548c4f654ad9
Tsur Benjamini <tsurbin@gmail.com>
2026-01-24 00:24:26 +0200

    remove redundant files and add a script to deploy the scraper

--- database/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Database layer for homework agent.
-Handles all database operations including DynamoDB access.
-"""
-from .dynamodb_handler import DynamoDBHandler
-
-__all__ = ['DynamoDBHandler']
